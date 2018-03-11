06b0257f45d817ecb95ee29aa6ed1c2226a50d50
Willy <willy.arisky@yahoo.de>
2018-03-11 12:07:30 +0700

    Add sweetalert2

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,15 @@ $(window).scroll(function() {
 });
 
 $(document).ready(function($){
+	function validateEmail(email) {
+	  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	  return re.test(email);
+	}
+
+	function doAlert($msg) {
+		swal('Oops!', $msg, 'warning');
+	}
+
   	$('.menu-icon').on('click', function(e){
     	e.preventDefault();
   	});
@@ -15,9 +24,34 @@ $(document).ready(function($){
 	  e.preventDefault();
 
 	  var $form = $(this);
-	  $.post($form.attr('action'), $form.serialize()).then(function() {
-	    alert("Thank you!");
-	  });
+	  var name = $('input[name="name"]').val();
+	  var email = $('input[name="email"]').val();
+	  var business = $('input[name="businessname"]').val();
+	  var phone = $('input[name="phonenumber"]').val();
+
+	  if( name == '' ) {
+	  	doAlert('Field name cannot be empty.');
+	  } else if( !validateEmail(email) ) {
+	  	doAlert('Your email address is invalid.');
+	  } else if( business != '' ) {
+	  	doAlert('Business name cannot be empty.');
+	  } else if( phone != '' ) {
+	  	doAlert('Phone number cannot be empty.');
+	  } else {
+	  	$.post($form.attr('action'), $form.serialize()).then(function() {
+			swal('Thank you', 'We will get back to you soon!', 'success');
+		});
+	  }
+
+	  
+	});
+
+	$(document).on('click', 'a[href^="#"]', function (event) {
+	    event.preventDefault();
+
+	    $('html, body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 500);
 	});
 	
 });
